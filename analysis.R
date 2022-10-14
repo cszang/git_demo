5c4fc7ef33f4ea2e44b59d1171fc915d0b189620
Christian Zang <cszang@users.noreply.github.com>
2022-10-14 11:37:57 +0200

    changed from procedural to functional

--- analysis.R
+++ analysis.R
@@ -1,10 +1,3 @@
-aq_months <- unique(airquality$Month)
-no_months <- length(aq_months)
-corels <- numeric(no_months)
-
-for (i in 1:no_months) {
-  this_month <- na.omit(airquality[airquality$Month == aq_months[i], ])
-  corels[i] <- cor(this_month$Ozone, this_month$Temp)
-}
-
-corels
+split(airquality, airquality$Month) |>
+  lapply(na.omit) |>
+  sapply(function(x) cor(x$Ozone, x$Temp))
